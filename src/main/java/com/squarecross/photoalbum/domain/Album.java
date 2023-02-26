b5ce03292329ea963b43e9cae79ab77c433d1a0e
odri19995 <odri19995@gmali.com>
2023-02-27 00:40:47 +0900

    make class Album (1)

---
+++ src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -0,0 +1,22 @@
+package com.squarecross.photoalbum.domain;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="album", schema="photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")})
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "album_id", unique = true, nullable = false)
+    private Long albumId;
+
+    @Column(name = "album_name", unique = false, nullable = false)
+    private String albumName;
+
+    @Column(name="created_at", unique = false, nullable = true)
+    @CreationTimestamp
+    private Date createdAt;
+
+}
